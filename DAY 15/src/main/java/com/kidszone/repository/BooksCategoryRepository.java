ecc77f0b1bf3ec02322ebdb23558664bbca7fba5
Srishankar123 <loknathsrishankar@gmail.com>
2023-10-09 11:42:22 +0530

    Delete

--- DAY 15/src/main/java/com/kidszone/repository/BooksCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kidszone.repository;
-
-import com.kidszone.entity.BooksCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooksCategoryRepository extends JpaRepository<BooksCategory,Integer> {
-}
--- DAY 15/src/main/java/com/kidszone/repository/BooksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kidszone.repository;
-
-import com.kidszone.entity.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooksRepository extends JpaRepository<Books,Long> {
-}
